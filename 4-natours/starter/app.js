0076eee80e28f4e030c0a4788251f5b000c9d865
JJWallis <josh.jamesonwallis@gmail.com>
2022-09-25 15:38:23 +0100

    natours 200 done

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -7,13 +7,37 @@ const tours = JSON.parse(
    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 )
 
-app.get('/api/v1/tours', (req, res) => {
+app.use(express.json())
+
+app.get('/api/v1/tours', (_, res) => {
    res.status(200).json({
       status: 'success',
+      results: tours.length, // only when sending arr -> easier for client
       data: {
          tours,
       },
    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+   const newId = tours[tours.length - 1].id + 1
+   const newTour = { id: newId, ...req.body }
+
+   tours.push(newTour) // reloads live server to get up-to-date data
+
+   fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(tours), // convert write data to file!
+      (err) => {
+         // successful resource creation code
+         res.status(201).json({
+            status: 'success',
+            data: {
+               tour: newTour, // send back data just created
+            },
+         })
+      }
+   )
 })
 
 app.listen(port, () => {
