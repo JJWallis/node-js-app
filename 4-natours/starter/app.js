129c8a255760b472ae86cc8fe7e45ae507ea7a75
JJWallis <josh.jamesonwallis@gmail.com>
2022-10-01 09:28:48 +0100

    url params done

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -19,6 +19,27 @@ app.get('/api/v1/tours', (_, res) => {
    })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+   console.log(req.params)
+   const targetId = parseInt(req.params.id)
+   const tour = tours.find(({ id }) => id === targetId)
+
+   if (tour) {
+      res.status(200).json({
+         status: 'success',
+         data: {
+            tour,
+         },
+      })
+      return
+   }
+
+   res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+   })
+})
+
 app.post('/api/v1/tours', (req, res) => {
    const newId = tours[tours.length - 1].id + 1
    const newTour = { id: newId, ...req.body }
