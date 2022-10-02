0079178dfbb7adb7f71e895e418b7984f36b926f
JJWallis <josh.jamesonwallis@gmail.com>
2022-10-02 08:10:06 +0100

    refactored

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -80,13 +80,15 @@ const deleteTour = (req, res) => {
 
 app.use(express.json())
 
-app.get('/api/v1/tours', getAllTours)
-app.get('/api/v1/tours/:id', getTour)
-app.post('/api/v1/tours', createTour)
-app.patch('/api/v1/tours/:id', updateTour)
-app.delete('/api/v1/tours/:id', deleteTour)
+// app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours/:id', getTour)
+// app.post('/api/v1/tours', createTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
 
-app.route('/api/v1/tours').get(getAllTours)
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
 app.listen(port, () => {
    console.log(`App running at http://localhost:${port}`)
