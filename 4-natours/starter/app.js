d933f4be4893457065d4cd131510a3567a669d06
JJWallis <josh.jamesonwallis@gmail.com>
2022-10-08 19:57:44 +0100

    tour controllers added

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,24 +3,11 @@ const morgan = require('morgan')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const app = express()
-const port = 3000
-
-// 1. MIDDLEWARE
 
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.use((req, res, next) => {
-   console.log('Hello from the middleware!')
-   next()
-   // CODE ORDER:
-   // if placed before controller -> be used for that route
-   // if placed after -> not because controller ends req/res cycle (send back res)
-})
-
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-app.listen(port, () => {
-   console.log(`App running at http://localhost:${port}`)
-})
+module.exports = app
---
+++ 4-natours/starter/controllers/tourController.js
@@ -0,0 +1,86 @@
+const fs = require('fs')
+
+const tours = JSON.parse(
+   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
+)
+
+const getAllTours = (req, res) => {
+   res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: tours.length, // only when sending arr -> easier for client
+      data: {
+         tours,
+      },
+   })
+}
+
+const getTour = (req, res) => {
+   console.log(req.params)
+   const targetId = parseInt(req.params.id)
+   const tour = tours.find(({ id }) => id === targetId)
+
+   if (tour) {
+      res.status(200).json({
+         status: 'success',
+         data: {
+            tour,
+         },
+      })
+      return
+   }
+
+   res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+   })
+}
+
+const createTour = (req, res) => {
+   const newId = tours[tours.length - 1].id + 1
+   const newTour = { id: newId, ...req.body }
+
+   tours.push(newTour) // reloads live server to get up-to-date data
+
+   fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(tours), // convert write data to file!
+      () => {
+         // successful resource creation code
+         res.status(201).json({
+            status: 'success',
+            data: {
+               tour: newTour, // send back data just created
+            },
+         })
+      }
+   )
+}
+
+const updateTour = (req, res) => {
+   if (parseInt(req.params.id) < tours.length) {
+      res.status(200).json({
+         status: 'success',
+         data: {
+            tour: 'Updated tour',
+         },
+      })
+   }
+}
+
+const deleteTour = (req, res) => {
+   if (parseInt(req.params.id) < tours.length) {
+      res.status(204).json({
+         status: 'success',
+         data: null,
+      })
+   }
+}
+
+module.exports = {
+   getAllTours,
+   getTour,
+   createTour,
+   updateTour,
+   deleteTour,
+}
