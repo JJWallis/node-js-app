0f0ba8899681f549fa4ac6a6aa83d64c11a23d56
JJWallis <josh.jamesonwallis@gmail.com>
2022-10-09 19:12:56 +0100

    validation fixed

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -5,7 +5,7 @@ const tours = JSON.parse(
 )
 
 const checkId = (_, res, next, val) => {
-   if (parseInt(val) < tours.length) {
+   if (parseInt(val) > tours.length) {
       return res.status(404).json({
          status: 'fail',
          message: 'Invalid ID',
